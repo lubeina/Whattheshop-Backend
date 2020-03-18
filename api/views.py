31a45901e11676e3224b0bafbeecda4e95f5e9c2
lubeina <52536093+lubeina@users.noreply.github.com>
2020-03-19 01:25:16 +0300

    adding response and status in views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, CreateAPIView
-from .serializers import UserCreateSerializer, CakeSerializer, CartSerializer, CartItemCreateSerializer, ProfileSerializer
+from .serializers import UserCreateSerializer, CakeSerializer, CartSerializer, CartItemCreateSerializer, ProfileSerializer,CartItemSerializer
 from .models import Cake, Cart_Item, Cart
 from django.contrib.auth.models import User
-
+from rest_framework.response import Response
+from rest_framework import status
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
