6498a291053c3bf51b563a4b2682dda1458c7d90
lubeina <52536093+lubeina@users.noreply.github.com>
2020-03-22 17:50:30 +0300

    cart_item changed to cartitem

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Cake, Cart, Cart_Item
+from .models import Cake, Cart, CartItem
 
 admin.site.register(Cake)
 admin.site.register(Cart)
-admin.site.register(Cart_Item)
+admin.site.register(CartItem)
