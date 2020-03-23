87bef4693ecf701ac74f6a7f3f0676a9b5ac588a
lubeina <52536093+lubeina@users.noreply.github.com>
2020-03-23 21:05:53 +0300

    changes in permissions

--- api/permissions.py
+++ api/permissions.py
@@ -4,7 +4,7 @@ class IsCartOwner(BasePermission):
 	message = "You must be the owner of this cart"
 
 	def has_object_permission(self, request, view, obj):
-		if request.user.is_staff or (obj.user == request.user):
+		if request.user.is_staff or (obj.cart.user == request.user):
 			return True
 		else:
 			return False
